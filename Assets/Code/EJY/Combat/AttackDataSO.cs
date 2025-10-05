4dfa8575cf86594a673fe6e2241de95cd8554b92
arke0909 <arke0909@gmail.com>
2025-10-05 22:10:48 +0900

    Feat : RangeEnemyAttackCompo

--- Assets/Code/EJY/Combat/AttackDataSO.cs
+++ Assets/Code/EJY/Combat/AttackDataSO.cs
@@ -8,12 +8,11 @@ namespace Code.Combat
     {
         public DamageType damageType = DamageType.MELEE;
         public string attackName;
-        public MovementDataSO movementData;
         public float damageMultiplier = 1f; //증가 뎀
         public float damageIncrease = 0;  //추가 뎀
         public bool isPowerAttack;
         public float impulseForce;
-
+ 
         private void OnEnable()
         {
             attackName = this.name; //파일 이름으로 공격 이름을 설정한다.
