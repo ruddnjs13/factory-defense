2d53c97e8e838379d35763a56fe438ec0fb68614
arke0909 <arke0909@gmail.com>
2025-10-31 12:46:46 +0900

    Feat : Fade Transition System

---
+++ Assets/Code/EJY/Events/SceneEvents.cs
@@ -0,0 +1,33 @@
+using Core.GameEvent;
+
+namespace Code.Events
+{
+    public static class SceneEvents
+    {
+        public static FadeEvent FadeEvent = new FadeEvent();
+        public static ChangeSceneEvent ChangeSceneEvent = new ChangeSceneEvent();
+    }
+    
+    public class FadeEvent : GameEvent
+    {
+        public bool isFadeIn;
+        public string sceneName;
+
+        public FadeEvent Init(bool isFadeIn, string sceneName)
+        {
+            this.isFadeIn = isFadeIn;
+            this.sceneName = sceneName;
+            return this;
+        }
+    }
+    
+    public class ChangeSceneEvent : GameEvent
+    {
+        public string sceneName;
+        public ChangeSceneEvent Init(string sceneName)
+        {
+            this.sceneName = sceneName;
+            return this;
+        }
+    }
+}
---
+++ Assets/Code/EJY/UI/FadeSystem/FadeCanvas.cs
@@ -0,0 +1,90 @@
+using Code.Events;
+using Core.GameEvent;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using UnityEngine.SceneManagement;
+
+namespace Code.UI.FadeSystem
+{
+    public class FadeCanvas : MonoBehaviour
+    {
+        [SerializeField] private Image fadeImage;
+        [SerializeField] private CanvasGroup canvasGroup;
+        [SerializeField] private GameEventChannelSO sceneChannel;
+        [SerializeField] private float duration = 3f;
+        [SerializeField] private bool inStartFadeIn;
+
+        private readonly int _scrollHash = Shader.PropertyToID("_Scroll");
+        private readonly int _rotateHash = Shader.PropertyToID("_Rotate"); 
+
+        private void Awake()
+        {
+            sceneChannel.AddListener<FadeEvent>(HandleFadeEvent);
+            SetCanvasGroup(false);
+        }
+
+        private void Start()
+        {
+            fadeImage.material = new Material(fadeImage.material);
+
+            if (inStartFadeIn)
+            {
+                Fade(false);
+            }
+        }
+
+        private void OnDestroy()
+        {
+            sceneChannel.RemoveListener<FadeEvent>(HandleFadeEvent);
+        }
+
+        private void HandleFadeEvent(FadeEvent evt)
+        {
+            Fade(evt.isFadeIn, evt.sceneName);
+        }
+
+        private void SetCanvasGroup(bool isStart)
+        {
+            canvasGroup.alpha = isStart ? 1 : 0;
+            canvasGroup.interactable = isStart;
+            canvasGroup.blocksRaycasts = isStart;
+        }
+
+        [ContextMenu("Test Fade In")]
+        public void FadeIn() => Fade(true);
+        [ContextMenu("Test Fade Out")]
+        public void FadeOut() => Fade(false);
+        
+        private void Fade(bool isFadeIn, string sceneName = null)
+        {
+            int startScrollValue = isFadeIn ? 0 : 4;
+            int endScrollValue = isFadeIn ? 4 : 0;
+            float startRotateValue = 0f;
+            float endRotateValue = 1.55f;
+            
+            fadeImage.material.SetFloat(_scrollHash, startScrollValue);
+            fadeImage.material.SetFloat(_rotateHash, startRotateValue);
+            
+            SetCanvasGroup(true);
+
+            Sequence seq = DOTween.Sequence();
+
+            seq.Append(fadeImage.material.DOFloat(endScrollValue, _scrollHash, duration)
+                    .SetUpdate(true)
+                    .OnComplete(() =>
+                    {
+                        if (isFadeIn && !string.IsNullOrEmpty(sceneName))
+                        {
+                            Time.timeScale = 1;
+                            SceneManager.LoadScene(sceneName);
+                        }
+                        else
+                        {
+                            SetCanvasGroup(false);
+                        }
+                    }))
+                .Join(fadeImage.material.DOFloat(endRotateValue, _rotateHash, duration).SetUpdate(true));
+        }
+    }
+}
